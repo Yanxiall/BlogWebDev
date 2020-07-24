a599d1c1cdaa6c9734ad67de6c91ac336c5879ad
Yanxiall <yanxia_he@126.com>
2020-07-24 15:28:17 +0200

    test the connetion with MySQL

--- src/test/java/com/HYX/webDev/MyBlogApplicationTests.java
+++ src/test/java/com/HYX/webDev/MyBlogApplicationTests.java
@@ -1,13 +1,26 @@
 package com.HYX.webDev;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class MyBlogApplicationTests {
-
+public class MyBlogApplicationTests {
+	@Autowired
+	private DataSource dataSource;
 	@Test
-	void contextLoads() {
+	public void datasourceTest() throws SQLException {
+		// create connection
+		Connection connection = dataSource.getConnection();
+		// to see if connection is null
+		System.out.println(connection != null);
+		connection.close();
 	}
-
 }
