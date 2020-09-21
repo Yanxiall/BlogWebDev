062a92e2dacc3092d01c67d4fed5dba1eef4719b
Yanxiall <yanxia_he@126.com>
2020-09-21 17:24:51 +0200

    download function

--- src/main/java/com/HYX/webDev/controller/blog/blogController.java
+++ src/main/java/com/HYX/webDev/controller/blog/blogController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import java.io.*;
 import java.util.Map;
 
 import static com.HYX.webDev.util.ResultGenerator.genSuccessResult;
@@ -160,5 +162,50 @@ public class blogController {
         }
         return "blog/blogdetail";
     }
-
+    @GetMapping("/download")
+    @ResponseBody
+    public String downloadFile(HttpServletRequest request, HttpServletResponse response) {
+        String fileName = "Yanxia_HE_CV.pdf";
+        if (fileName != null) {
+            //set file path
+            File file = new File("/home/hadoop/IdeaProjects/BlogWebDev/src/main/resources/static/blog/Yanxia_HE_CV.pdf");
+            if (file.exists()) {
+                System.out.println("come in");
+                response.setContentType("application/force-download");// force download without opening
+                response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// set file name
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    return "Download successed!!";
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return "Download Fail";
+    }
 }
